037cf3131da6f51a340d71b666efe4b2271264d5
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-27 23:21:48 -0500

    adds basics

---
+++ Udemy/learnc++/C++ Basics/skeleton.cpp
@@ -0,0 +1,22 @@
+// External Libraries:
+#include <iostream>
+using namespace std;
+
+// First Program in the Course:
+
+// Starting point of a C++ Function
+
+// Written Type Function: return 0
+int main()
+{
+    // Body of the function
+
+    // Printing in C++
+
+    // std::is a scope resolution and must be used unless you use the "using namespace std" as written above
+
+    // console out:
+    cout << "Hello World!";
+
+    return 0;
+}
