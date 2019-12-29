cb8346ed9084606b5581fb1da5d0ee2ed30a853a
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-28 20:49:58 -0500

    adds AreaofTriangle program

---
+++ Udemy/learnc++/C++ Basics/BasicPrograms/AreaofTriangle.cpp
@@ -0,0 +1,34 @@
+// BUILD PROGRAM:
+// ==============
+// c++ -c AreaofTriangle.cpp
+// c++ AreaofTrigangle.o
+
+// EXECUTE PROGRAM:
+// ===============
+//  ./a.out
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// This program is used for calculating the area of a triangle from user input
+int main()
+{
+    // Equation:
+    // Area = (base * height) / 2
+
+    double Area;
+    int base, height;
+
+    // Prompt User for Data:
+    cout << "Enter Base and Height of Triangle:";
+
+    // Get the variables of the triangle
+    cin >> base >> height;
+
+    Area = (float)(base * height) / 2;
+
+    cout << "The Calculated Area is " << Area;
+
+    // Displays the Area of the Triangle
+}
