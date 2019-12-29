dead80e6b9c3b73dcdf125688bd7a74749f36656
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-28 20:59:21 -0500

    intial commit for sumofNaturalNums

---
+++ Udemy/learnc++/C++ Basics/BasicPrograms/sumofNaturalNums.cpp
@@ -0,0 +1,16 @@
+// BUILD PROGRAM:
+// ==============
+// c++ -c sumofNatrualNums.cpp
+// c++ sumofNaturalNums.o
+
+// EXECUTE PROGRAM:
+// ===============
+//  ./a.out
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    
+}
