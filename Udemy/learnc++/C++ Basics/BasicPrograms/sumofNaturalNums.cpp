c9bf7a00ecd1c87cfaf4a4f2f73616e1a2317b6f
GoodGuyGregory <greg.witt625@gmail.com>
2020-01-01 21:43:01 -0500

    adds natural sum formula

--- Udemy/learnc++/C++ Basics/BasicPrograms/sumofNaturalNums.cpp
+++ Udemy/learnc++/C++ Basics/BasicPrograms/sumofNaturalNums.cpp
@@ -7,10 +7,24 @@
 // ===============
 //  ./a.out
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-    
+    // declare variable from the user:
+    int n, sum;
+
+    cout << "Enter Number: ";
+
+    cin >> n;
+
+    // Now Calculate the Natural Sum of N from the equation:
+    // sum = (n * (n + 1))/2;
+    sum = (n * (n + 1)) / 2;
+
+    cout << "The Natural Sum of N is: " << sum;
+
+    // End the Program
+    return 0;
 }
