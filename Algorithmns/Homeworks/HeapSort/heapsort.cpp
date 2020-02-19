5026f3f2ac567907fdd39a87dfde0ecefc69e022
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-18 22:09:41 -0500

    adds heapsort

--- Algorithmns/Homeworks/HeapSort/heapsort.cpp
+++ Algorithmns/Homeworks/HeapSort/heapsort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void displayArray(int heap[], int sizeh)
 {
-    cout << "Array:" << endl;
+
     for (int i = 0; i < sizeh; i++)
         cout << heap[i] << " ";
     cout << "\n";
@@ -34,15 +34,14 @@ void heapify(int heap[], int sizeh, int i)
         swap(heap[i], heap[largestNode]);
         heapify(heap, sizeh, largestNode);
     }
+    
 }
-
-// Builds a Heao with Supporting Algorithm:
-// See notes:
 
 void buildHeap(int heap[], int sizeh)
 {
     for (int i = int((sizeh / 2) - 1); i >= 0; i--)
     {
+
         heapify(heap, sizeh, i);
     }
 }
@@ -51,14 +50,18 @@ void buildHeap(int heap[], int sizeh)
 void heapSort(int heap[], int sizeh)
 {
 
+    cout << "Calling Build Heap" << endl;
     buildHeap(heap, sizeh);
 
     // Display the Array Prior to Sorting
     displayArray(heap, sizeh);
+
     for (int i = (sizeh - 1); i >= 2; i--)
     {
-        swap(heap[0], heap[i]);
 
+        swap(heap[0], heap[i]);
+        cout << "calling Heapify for Array:" << endl;
+        displayArray(heap, sizeh);
         heapify(heap, i, 0);
     }
 }
@@ -66,15 +69,25 @@ void heapSort(int heap[], int sizeh)
 int main()
 {
     // Variables for Heapify
-    int heap[] = {9, 21, 23, 64, 8, 42, 52, 26, 72, 36};
-    int length = sizeof(heap) / sizeof(heap[0]);
+    int A1[] = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+    int firstLength = sizeof(A1) / sizeof(A1[0]);
 
-    // Display pre-sorted Array:
-    cout << "Array BEFORE sorting" << endl;
-    displayArray(heap, length);
+    int A2[] = {13, 2, 91, 102, 12, 87, 45, 39, 27, 55, 42, 11, 3, 18, 64, 75, 51};
+    int secondLength = sizeof(A2) / sizeof(A2[0]);
 
-    heapSort(heap, length);
+    cout << "A1 BEFORE sorting" << endl;
+    displayArray(A1, firstLength);
 
-    cout << "Array AFTER heapify" << endl;
-    displayArray(heap, length);
+    heapSort(A1, firstLength);
+
+    cout << "A1 AFTER heapify" << endl;
+    displayArray(A1, firstLength);
+
+    cout << "A2 BEFORE sorting" << endl;
+    displayArray(A2, secondLength);
+
+    heapSort(A2, secondLength);
+
+    cout << "A2 AFTER heapify" << endl;
+    displayArray(A2, secondLength);
 }
