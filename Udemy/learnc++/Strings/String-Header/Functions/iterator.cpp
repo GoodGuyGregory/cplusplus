cef30256a4a063e09bd1310a3f53cc119adf7351
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-09 16:02:35 -0500

    adds basic itertion technique for strings

--- Udemy/learnc++/Strings/String-Header/Functions/iterator.cpp
+++ Udemy/learnc++/Strings/String-Header/Functions/iterator.cpp
@@ -9,10 +9,22 @@ int main() {
 
   string::iterator it;
 
+  // Iterator Approach:
   for (it=str.begin(); it != str.end(); it++) {
     //  subtract 32 from the ASCII value of the string
     *it = *it-32;
   }
+
+  cout << str << endl;
+
+  cout << "basic iteration without an iterator object" << endl;
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    cout << str[i] << endl;
+    str[i] += 32;
+  }
+
+  cout << "Removes the Capital Letters" << endl;
 
   cout << str << endl;
 }
