d0595ae80decb896954ecdc59c39e7c86ab7e483
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-14 12:55:11 -0500

    adds length of String challenge

---
+++ Udemy/learnc++/Strings/Practice/lengthOfString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string challengeString = "Welcome";
+
+  cout << "Find the length of the string: " << challengeString << endl;
+
+  cout << "The length of the string is: " << challengeString.length();
+
+}
