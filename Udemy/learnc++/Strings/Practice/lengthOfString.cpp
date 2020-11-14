9d456aa69e8e1c4e4fe4dd195188adfd55d94220
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-14 13:00:12 -0500

    adds length of String

--- Udemy/learnc++/Strings/Practice/lengthOfString.cpp
+++ Udemy/learnc++/Strings/Practice/lengthOfString.cpp
@@ -7,6 +7,13 @@ int main() {
 
   cout << "Find the length of the string: " << challengeString << endl;
 
-  cout << "The length of the string is: " << challengeString.length();
+  cout << "The length of the string is: " << challengeString.length() << endl;
+
+  // Access all letters of the string:
+  for (int i = 0; challengeString[i] != '\0'; i++) {
+    cout << challengeString[i] << endl;
+  }
+
+  return 0;
 
 }
